e050d23edd5a54ef494056c487f6d62828862a7d
liliia-r <liliaglushko1992@gmail.com>
2020-04-21 10:38:26 +0300

    Updates

---
+++ precache-manifest.ff91b2f687a8a228a02bd2f5d4dc4f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd32f856fbe949bfc7b9e0cb09f120b6",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "abad1a86e1c8d4968d76",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "abad1a86e1c8d4968d76",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0751b791.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0751b791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a20dde1a9b33ee364c",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.b2f30192.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
